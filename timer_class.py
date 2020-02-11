7c4b9b570b1bfcd513835490f929daac0cb78640
BALMATAZAR <57841120+BALMATAZAR@users.noreply.github.com>
2020-02-11 19:56:45 +0300

    Update timer_class.py

--- timer_class.py
+++ timer_class.py
@@ -1,8 +1,11 @@
+# импорт модуля
+
 import time
 
+# Создание класса
 class timeclock():
 	def __init__(self, function):
-		self.tun = function
+		self.run = function
 
 	def timer(self, *args, trying = 1):
 		self.trying = trying
@@ -14,3 +17,9 @@ class timeclock():
 		print("Количество выполнений скрипта {}".format(self.trying))
 		print("Общее время выполнения - %.5f секунд" % self.avg_timer)
 		print("Среднее время выполнения %.5f секунд" % (self.avg_timer / self.trying))
+	
+@timeclock
+def test(a, b):
+    print("This is a - {}".format(a))
+    print("This is b - {}".format(b))
+test.timer("first argument (a)", "second argument (b)", trying = 5)
